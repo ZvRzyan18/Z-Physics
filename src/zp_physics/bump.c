3fc8a8705ad24133c029e5edb1cf38e1870d27b8
ZvRzyan18 <rhenzalfred06@gmail.com>
2026-09-16 17:20:18 +0800

    Create bump.c

---
+++ src/zp_physics/bump.c
@@ -0,0 +1,41 @@
+#include "zp_physics/bump.h"
+#include "zp_physics/math.h"
+#include <stdlib.h>
+#include <assert.h>
+
+/*
+ bump/stack-based allocator
+*/
+zp_cold int zp_bump_init(zp_bump *zp_restrict b, size_t max_size) {
+ b->_bytes = (uint8_t*)malloc(max_size);
+ b->_max_size = max_size;
+ b->_size = 0;
+ 
+ if(zp_unlikely(!b->_bytes))
+  return -1;
+ return 0;
+}
+
+
+zp_cold void zp_bump_destroy(zp_bump *zp_restrict b) {
+ free(b->_bytes);
+}
+
+
+size_t zp_bump_acquire(zp_bump *zp_restrict b, uint16_t requested_bytes, uint8_t alignment) {
+ size_t current_pointer;
+ current_pointer = zp_alignto(b->_size, alignment);
+ b->_size = current_pointer + requested_bytes;
+ assert(b->_size < b->_max_size);
+ return current_pointer;
+}
+
+
+void zp_bump_reset(zp_bump *zp_restrict b, size_t offset) {
+ b->_size = offset;
+ assert(b->_size < b->_max_size);
+}
+
+
+
+
